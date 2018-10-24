8607ee2a663b05252d116e91b1148bc9b1dce566
James Melton <james09melton@gmail.com>
2018-10-24 09:14:19 -0500

    arrays

---
+++ Project/src/com/fundamentals/java/Bicycle.java
@@ -0,0 +1,22 @@
+package com.fundamentals.java;
+
+public class Bicycle {
+
+	public String pedals;
+	
+	public String handlebars;
+	
+	public String brakes;
+	
+	public void pedaling() {
+	
+	}
+	
+	public void steering() {
+		
+	}
+	
+	public void braking() {
+		
+	}
+}
